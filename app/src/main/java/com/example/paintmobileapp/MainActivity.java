796ca4684e6429f5afc5999e21fc8c2302c76ded
ne <145484437+nebyui@users.noreply.github.com>
2024-11-02 20:12:47 -0600

    app update

--- app/src/main/java/com/example/paintmobileapp/MainActivity.java
+++ app/src/main/java/com/example/paintmobileapp/MainActivity.java
@@ -1,42 +1,22 @@
 package com.example.paintmobileapp;
 
+import android.graphics.Canvas;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private CanvasView canvasView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        canvasView = findViewById(R.id.canvasView);
 
-        Button button = findViewById(R.id.button);
-        TextView textView = findViewById(R.id.textView);
-
-
-
-
-
-
-
-
-
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
